b1eced950cbb507c1ddd75732d8d9b98807b2a6a
usn757 <usn757@gmail.com>
2025-07-06 22:12:40 +0900

    refactor: 활성 사용자 수 확인 중복 제거

--- src/main/java/com/team03/ticketmon/queue/service/WaitingQueueService.java
+++ src/main/java/com/team03/ticketmon/queue/service/WaitingQueueService.java
@@ -2,7 +2,6 @@ package com.team03.ticketmon.queue.service;
 
 import com.team03.ticketmon._global.exception.BusinessException;
 import com.team03.ticketmon._global.exception.ErrorCode;
-import com.team03.ticketmon.concert.service.ConcertService;
 import com.team03.ticketmon.queue.adapter.QueueRedisAdapter;
 import com.team03.ticketmon.queue.dto.QueueStatusDto;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +16,8 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Redis Sorted Set을 이용해 콘서트 대기열을 관리하는 서비스입니다.
- * 이 서비스는 대기열 추가, 순위 조회, 사용자 추출 등의 핵심 기능을 담당하며,
+ * Redis Sorted Set을 이용해 콘서트 대기열을 관리하는 서비스
+ * 이 서비스는 대기열 추가, 순위 조회, 사용자 추출 등의 핵심 기능을 담당
  * 모든 연산은 원자성(Atomic)을 보장해야 합니다.
  */
 @Slf4j
@@ -26,7 +25,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WaitingQueueService {
 
-    private final ConcertService concertService;
     private final AdmissionService admissionService;
     private final QueueRedisAdapter queueRedisAdapter;
 
@@ -42,20 +40,16 @@ public class WaitingQueueService {
      * @return 1부터 시작하는 사용자의 대기 순번
      */
     public QueueStatusDto apply(Long concertId, Long userId) {
-        // 1. 현재 활성 사용자 수를 확인하여 즉시 입장을 시도할지 결정
-        long currentActiveUsers = queueRedisAdapter.getActiveUserCounter(concertId).get();
 
-        // 2. 자리가 남아있다고 판단되면, '원자적 슬롯 점유'를 시도
-        if (currentActiveUsers < maxActiveUsers) {
-            if (admissionService.tryClaimSlot(concertId)) {
-                log.debug("[userId: {}] 즉시 입장 처리 시작.", userId);
-                // 2-1. 슬롯 점유에 성공! -> 즉시 입장 처리
-                String accessKey = admissionService.grantAccess(concertId, userId);
-                return QueueStatusDto.immediateEntry(accessKey);
-            }
+        // 1. 원자적 슬롯 점유 시도
+        if (admissionService.tryClaimSlot(concertId)) {
+            log.debug("[userId: {}] 즉시 입장 처리 시작.", userId);
+            String accessKey = admissionService.grantAccess(concertId, userId);
+            return QueueStatusDto.immediateEntry(accessKey);
         }
 
-        // 3. 슬롯이 꽉 찼거나, 점유 시도에 실패(경쟁에서 밀림)하면 대기열로 진입
+        // 2. 슬롯이 꽉 찼거나, 점유 시도에 실패(경쟁에서 밀림)하면 대기열로 진입
+        log.debug("사용자 {} 대기열 진입 처리 시작.", userId);
         RScoredSortedSet<Long> queue = queueRedisAdapter.getQueue(concertId);
         long uniqueScore = queueRedisAdapter.generateQueueScore(concertId);
 
